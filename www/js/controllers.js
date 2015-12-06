0397aa7d1989ae15d376ee5ba002bfc43adbd748
elizabethhu <elizabethhu3@gmail.com>
2015-12-05 17:21:17 -0800

    hello nancy

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,61 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('AppCtrl', function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
+	  $scope.username = AuthService.username();
+	 
+	  $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
+	    var alertPopup = $ionicPopup.alert({
+	      title: 'Unauthorized!',
+	      template: 'You are not allowed to access this resource.'
+	    });
+	  });
+	 
+	  $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
+	    AuthService.logout();
+	    $state.go('login');
+	    var alertPopup = $ionicPopup.alert({
+	      title: 'Session Lost!',
+	      template: 'Sorry, You have to login again.'
+	    });
+	  });
+	 
+	  $scope.setCurrentUsername = function(name) {
+	    $scope.username = name;
+	  };
+	})
 
+
+.controller('DashCtrl', function($scope, $state, $http, $ionicPopup, AuthService) {
+  $scope.logout = function() {
+    AuthService.logout();
+    $state.go('login');
+  };
+ 
+  $scope.performValidRequest = function() {
+    $http.get('http://localhost:8100/valid').then(
+      function(result) {
+        $scope.response = result;
+      });
+  };
+ 
+  $scope.performUnauthorizedRequest = function() {
+    $http.get('http://localhost:8100/notauthorized').then(
+      function(result) {
+        // No result here..
+      }, function(err) {
+        $scope.response = err;
+      });
+  };
+ 
+  $scope.performInvalidRequest = function() {
+    $http.get('http://localhost:8100/notauthenticated').then(
+      function(result) {
+        // No result here..
+      }, function(err) {
+        $scope.response = err;
+      });
+  };
+})
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -21,8 +75,27 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+.controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService) {
+  $scope.data = {};
+ 
+  $scope.login = function(data) {
+    AuthService.login(data.username, data.password).then(function(authenticated) {
+      $state.go('main.dash', {}, {reload: true});
+      $scope.setCurrentUsername(data.username);
+    }, function(err) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login failed!',
+        template: 'Please check your credentials!'
+      });
+    });
+  };
+})
+
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
 });
+
+
